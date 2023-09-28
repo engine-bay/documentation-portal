dd49231178ea319e7e950ff67f9cb151ecc102d9
Matthew van der Velden <matthew.vdvelden@entlect.co.uk>
2023-09-28 09:30:47 +0100

    #major upgrade of EngineBay.Core version

--- EngineBay.DocumentationPortal/DocumentationPortalModule.cs
+++ EngineBay.DocumentationPortal/DocumentationPortalModule.cs
@@ -5,21 +5,9 @@ namespace EngineBay.DocumentationPortal
     using EngineBay.Core;
     using Microsoft.Extensions.FileProviders;
 
-    public class DocumentationPortalModule : IModule
+    public class DocumentationPortalModule : BaseModule
     {
-        /// <inheritdoc/>
-        public IServiceCollection RegisterModule(IServiceCollection services, IConfiguration configuration)
-        {
-            return services;
-        }
-
-        /// <inheritdoc/>
-        public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints)
-        {
-            return endpoints;
-        }
-
-        public WebApplication AddMiddleware(WebApplication app)
+        public override WebApplication AddMiddleware(WebApplication app)
         {
             var currentAssembly = Assembly.GetExecutingAssembly();
             if (currentAssembly is null)
