7e56d45de81c6c38655da3020405aa56f0edb831
Janus Liebenberg <janus.liebenberg@entelect.co.uk>
2023-11-22 15:33:20 +0000

    add single test

---
+++ EngineBay.DocumentationPortal.Tests/DocumentationPortalTests.cs
@@ -0,0 +1,13 @@
+namespace EngineBay.DocumentationPortal.Tests
+{
+    using Xunit;
+
+    public class DocumentationPortalTests
+    {
+        [Fact]
+        public void ControlTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
